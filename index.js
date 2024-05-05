b370c6430eec3d635e522d1b879b7b44f8f83c6e
Ankit Panda <ankitpanda@Ankits-MacBook-Air.local>
2024-05-05 17:56:47 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 const {MONGOURI} = require("./keys")
 const mongoose = require('mongoose')
-const PORT = 8000;
+const PORT = process.env.PORT;
 const cors = require('cors')
 
 
--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
+const dotenv = require('dotenv').config();
 module.exports = {
-    MONGOURI: "mongodb+srv://ankitpanda1612:JY4Ez4ZpSGP0S4HF@cluster0.1owlvj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+   
+    MONGOURI: process.env.MONGOURI,
     JWT_SECRET: "aldiajdfnamnslallahdnsfin"
 }
